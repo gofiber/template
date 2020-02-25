ba25c2f35859ffcaa9636dcf33c1c5041b6fcff0
Fenny <fenny@protonmail.com>
2020-02-25 04:28:05 +0100

    v1.0.0

--- template.go
+++ template.go
@@ -3,11 +3,11 @@ package template
 import (
 	"bytes"
 	"html/template"
+
 	pug "github.com/Joker/jade"
 	handlebars "github.com/aymerick/raymond"
 	mustache "github.com/cbroglie/mustache"
 	amber "github.com/eknkc/amber"
-	// "github.com/gofiber/jet"
 )
 
 // Amber https://github.com/eknkc/amber
@@ -16,14 +16,14 @@ func Amber(raw string, binding interface{}) (out string, err error) {
 	var tmpl *template.Template
 
 	if tmpl, err = amber.Compile(raw, amber.DefaultOptions); err != nil {
-		return out, err
+		return
 	}
 	if err = tmpl.Execute(&buf, binding); err != nil {
-		return out, err
+		return
 	}
 	out = buf.String()
 
-	return out, err
+	return
 }
 
 // Handlebars https://github.com/aymerick/raymond
@@ -37,37 +37,35 @@ func HTML(raw string, binding interface{}) (out string, err error) {
 	var tmpl *template.Template
 
 	if tmpl, err = template.New("").Parse(raw); err != nil {
-		return out, err
+		return
 	}
 	if err = tmpl.Execute(&buf, binding); err != nil {
-		return out, err
+		return
 	}
 	out = buf.String()
-
-	return out, err
+	return
 }
 
 // Mustache https://github.com/hoisie/mustache
-func Mustache(raw string, binding interface{}) (html string, err error) {
+func Mustache(raw string, binding interface{}) (out string, err error) {
 	return mustache.Render(raw, binding)
 }
 
 // Pug https://github.com/Joker/jade
-func Pug(raw string, binding interface{}) (html string, err error) {
+func Pug(raw string, binding interface{}) (out string, err error) {
 	var buf bytes.Buffer
 	var tmpl *template.Template
 
 	if raw, err = pug.Parse("", []byte(raw)); err != nil {
-		return html, err
+		return
 	}
 	if tmpl, err = template.New("").Parse(raw); err != nil {
-		return html, err
+		return
 	}
 	if err = tmpl.Execute(&buf, binding); err != nil {
-		return html, err
+		return
 	}
-	html = buf.String()
-
-	return html, err
+	out = buf.String()
+	return
 
 }
