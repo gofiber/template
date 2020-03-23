e59785739ce5ecd8305ff438ba9575b00ef0a7b6
Fenny <25108519+Fenny@users.noreply.github.com>
2020-03-23 17:59:05 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,3 @@
-// 🚀 Fiber is an Express inspired web framework written in Go with 💖
-// 📌 API Documentation: https://fiber.wiki
-// 📝 Github Repository: https://github.com/gofiber/fiber
-
 package template
 
 import (
@@ -15,69 +11,61 @@ import (
 )
 
 // Amber https://github.com/eknkc/amber
-func Amber() func(raw string, bind interface{}) (out string, err error) {
-	return func(raw string, bind interface{}) (out string, err error) {
-		var buf bytes.Buffer
-		var tmpl *template.Template
+func Amber(raw string, binding interface{}) (out string, err error) {
+	var buf bytes.Buffer
+	var tmpl *template.Template
 
-		if tmpl, err = amber.Compile(raw, amber.DefaultOptions); err != nil {
-			return
-		}
-		if err = tmpl.Execute(&buf, bind); err != nil {
-			return
-		}
-		out = buf.String()
+	if tmpl, err = amber.Compile(raw, amber.DefaultOptions); err != nil {
 		return
 	}
+	if err = tmpl.Execute(&buf, binding); err != nil {
+		return
+	}
+	out = buf.String()
+
+	return
 }
 
 // Handlebars https://github.com/aymerick/raymond
-func Handlebars() func(raw string, bind interface{}) (out string, err error) {
-	return func(raw string, bind interface{}) (out string, err error) {
-		return handlebars.Render(raw, bind)
-	}
+func Handlebars(raw string, data interface{}) (out string, err error) {
+	return handlebars.Render(raw, data)
 }
 
 // HTML https://golang.org/pkg/text/template/
-func HTML() func(raw string, bind interface{}) (out string, err error) {
-	return func(raw string, bind interface{}) (out string, err error) {
-		var buf bytes.Buffer
-		var tmpl *template.Template
+func HTML(raw string, binding interface{}) (out string, err error) {
+	var buf bytes.Buffer
+	var tmpl *template.Template
 
-		if tmpl, err = template.New("").Parse(raw); err != nil {
-			return
-		}
-		if err = tmpl.Execute(&buf, bind); err != nil {
-			return
-		}
-		out = buf.String()
+	if tmpl, err = template.New("").Parse(raw); err != nil {
 		return
 	}
+	if err = tmpl.Execute(&buf, binding); err != nil {
+		return
+	}
+	out = buf.String()
+	return
 }
 
 // Mustache https://github.com/hoisie/mustache
-func Mustache() func(raw string, bind interface{}) (out string, err error) {
-	return func(raw string, bind interface{}) (out string, err error) {
-		return mustache.Render(raw, bind)
-	}
+func Mustache(raw string, binding interface{}) (out string, err error) {
+	return mustache.Render(raw, binding)
 }
 
 // Pug https://github.com/Joker/jade
-func Pug() func(raw string, bind interface{}) (out string, err error) {
-	return func(raw string, bind interface{}) (out string, err error) {
-		var buf bytes.Buffer
-		var tmpl *template.Template
+func Pug(raw string, binding interface{}) (out string, err error) {
+	var buf bytes.Buffer
+	var tmpl *template.Template
 
-		if raw, err = pug.Parse("", []byte(raw)); err != nil {
-			return
-		}
-		if tmpl, err = template.New("").Parse(raw); err != nil {
-			return
-		}
-		if err = tmpl.Execute(&buf, bind); err != nil {
-			return
-		}
-		out = buf.String()
+	if raw, err = pug.Parse("", []byte(raw)); err != nil {
 		return
 	}
+	if tmpl, err = template.New("").Parse(raw); err != nil {
+		return
+	}
+	if err = tmpl.Execute(&buf, binding); err != nil {
+		return
+	}
+	out = buf.String()
+	return
+
 }
