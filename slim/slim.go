5cac05a4c5307653e1a7a35fc688ebba5fdb6213
Fenny <25108519+Fenny@users.noreply.github.com>
2020-06-17 12:45:45 +0200

    Add Slim Engine

---
+++ slim/slim.go
@@ -0,0 +1,203 @@
+package slim
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+
+	"github.com/gofiber/template/utils"
+	"github.com/mattn/go-slim"
+)
+
+// Engine struct
+type Engine struct {
+	// delimiters
+	left  string
+	right string
+	// views folder
+	directory string
+	// http.FileSystem supports embedded files
+	fileSystem http.FileSystem
+	// views extension
+	extension string
+	// layout variable name that incapsulates the template
+	layout string
+	// reload on each render
+	reload bool
+	// debug prints the parsed templates
+	debug bool
+	// lock for funcmap and templates
+	mutex sync.RWMutex
+	// template funcmap
+	funcmap map[string]interface{}
+	// templates
+	Templates map[string]*slim.Template
+}
+
+// New returns a Handlebar render engine for Fiber
+func New(directory, extension string) *Engine {
+	engine := &Engine{
+		left:      "{{",
+		right:     "}}",
+		directory: directory,
+		extension: extension,
+		layout:    "embed",
+		funcmap:   make(map[string]interface{}),
+	}
+	return engine
+}
+
+func NewFileSystem(fs http.FileSystem, extension string) *Engine {
+	engine := &Engine{
+		left:       "{{",
+		right:      "}}",
+		directory:  "/",
+		fileSystem: fs,
+		extension:  extension,
+		layout:     "embed",
+		funcmap:    make(map[string]interface{}),
+	}
+	return engine
+}
+
+// Layout defines the variable name that will incapsulate the template
+func (e *Engine) Layout(key string) *Engine {
+	e.layout = key
+	return e
+}
+
+// Delims sets the action delimiters to the specified strings, to be used in
+// templates. An empty delimiter stands for the
+// corresponding default: {{ or }}.
+func (e *Engine) Delims(left, right string) *Engine {
+	fmt.Println("delims: this method is not supported for django")
+	return e
+}
+
+// AddFunc adds the function to the template's function map.
+// It is legal to overwrite elements of the default actions
+func (e *Engine) AddFunc(name string, fn interface{}) *Engine {
+	e.mutex.Lock()
+	e.funcmap[name] = fn
+	e.mutex.Unlock()
+	return e
+}
+
+// Reload if set to true the templates are reloading on each render,
+// use it when you're in development and you don't want to restart
+// the application when you edit a template file.
+func (e *Engine) Reload(enabled bool) *Engine {
+	e.reload = enabled
+	return e
+}
+
+// Debug will print the parsed templates when Load is triggered.
+func (e *Engine) Debug(enabled bool) *Engine {
+	e.debug = enabled
+	return e
+}
+
+// Parse is deprecated, please use Load() instead
+func (e *Engine) Parse() error {
+	fmt.Println("Parse() is deprecated, please use Load() instead.")
+	return e.Load()
+}
+
+// Load parses the templates to the engine.
+func (e *Engine) Load() error {
+	// race safe
+	e.mutex.Lock()
+	defer e.mutex.Unlock()
+
+	e.Templates = make(map[string]*slim.Template)
+
+	// Loop trough each directory and register template files
+	walkFn := func(path string, info os.FileInfo, err error) error {
+		// Return error if exist
+		if err != nil {
+			return err
+		}
+		// Skip file if it's a directory or has no file info
+		if info == nil || info.IsDir() {
+			return nil
+		}
+		// Get file extension of file
+		ext := filepath.Ext(path)
+		// Skip file if it does not equal the given template extension
+		if ext != e.extension {
+			return nil
+		}
+		// Get the relative file path
+		// ./views/html/index.tmpl -> index.tmpl
+		rel, err := filepath.Rel(e.directory, path)
+		if err != nil {
+			return err
+		}
+		// Reverse slashes '\' -> '/' and
+		// partials\footer.tmpl -> partials/footer.tmpl
+		name := filepath.ToSlash(rel)
+		// Remove ext from name 'index.tmpl' -> 'index'
+		name = strings.Replace(name, e.extension, "", -1)
+		// Read the file
+		// #gosec G304
+		buf, err := utils.ReadFile(path, e.fileSystem)
+		if err != nil {
+			return err
+		}
+		// Create new template associated with the current one
+		tmpl, err := slim.Parse(bytes.NewReader(buf))
+		if err != nil {
+			return err
+		}
+		// tmpl.FuncMap(e.funcmap)
+		e.Templates[name] = tmpl
+		// Debugging
+		if e.debug {
+			fmt.Printf("views: parsed template: %s\n", name)
+		}
+		return err
+	}
+	if e.fileSystem != nil {
+		return utils.Walk(e.fileSystem, e.directory, walkFn)
+	}
+	return filepath.Walk(e.directory, walkFn)
+}
+
+// Execute will render the template by name
+func (e *Engine) Render(out io.Writer, template string, binding interface{}, layout ...string) error {
+	if e.reload {
+		if err := e.Load(); err != nil {
+			return err
+		}
+	}
+	tmpl, ok := e.Templates[template]
+	if !ok {
+		return fmt.Errorf("template %s does not exist", template)
+	}
+	return tmpl.Execute(out, binding)
+	// bind := getPongoBinding(binding)
+	// parsed, err := tmpl.Execute(bind)
+	// if err != nil {
+	// 	return err
+	// }
+	// if len(layout) > 0 {
+	// 	if bind == nil {
+	// 		bind = make(map[string]interface{}, 1)
+	// 	}
+	// 	bind[e.layout] = parsed
+	// 	lay := e.Templates[layout[0]]
+	// 	if lay == nil {
+	// 		return fmt.Errorf("layout %s does not exist", layout[0])
+	// 	}
+	// 	return lay.ExecuteWriter(bind, out)
+	// }
+	// if _, err = out.Write([]byte(parsed)); err != nil {
+	// 	return err
+	// }
+	// return nil
+}
