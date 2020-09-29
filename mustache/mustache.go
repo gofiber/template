e10dd6e2a0d22f0bcb697e08119c3592358325e0
Fenny <25108519+Fenny@users.noreply.github.com>
2020-09-30 00:07:35 +0200

    🩹 fix partial clone err

--- mustache/mustache.go
+++ mustache/mustache.go
@@ -157,6 +157,9 @@ func (e *Engine) Load() error {
 // Execute will render the template by name
 func (e *Engine) Render(out io.Writer, template string, binding interface{}, layout ...string) error {
 	if !e.loaded || e.reload {
+		if e.reload {
+			e.loaded = false
+		}
 		if err := e.Load(); err != nil {
 			return err
 		}
