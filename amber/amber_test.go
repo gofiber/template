562cd56e7cfcee943569ef2177cb90aac7962c18
MINGKYME <admin@mingky.me>
2022-05-19 17:31:02 +0900

    Remove embed test code

--- amber/amber_test.go
+++ amber/amber_test.go
@@ -2,7 +2,6 @@ package amber
 
 import (
 	"bytes"
-	"embed"
 	"io/ioutil"
 	"net/http"
 	"regexp"
@@ -106,28 +105,7 @@ func Test_FileSystem(t *testing.T) {
 		t.Fatalf("Expected:\n%s\nResult:\n%s\n", expect, result)
 	}
 }
-//go:embed embed_views/*
-var embededViews embed.FS
-func Test_EmbedFileSystem(t *testing.T) {
-	engine := NewFileSystem(http.FS(embededViews), ".amber")
-	engine.Debug(true)
-	if err := engine.Load(); err != nil {
-		t.Fatalf("load: %v\n", err)
-	}
 
-	var buf bytes.Buffer
-	err := engine.Render(&buf, "embed_views/embed", map[string]interface{}{
-		"Title": "Hello, World!",
-	}, "embed_views/layouts/main")
-	if err != nil {
-		t.Fatalf("render: %v", err)
-	}
-	expect := `<!DOCTYPE html><html><head><title>Main</title></head><body><h2>Header</h2><h1>Hello, World!</h1><h2>Footer</h2></body></html>`
-	result := trim(buf.String())
-	if expect != result {
-		t.Fatalf("Expected:\n%s\nResult:\n%s\n", expect, result)
-	}
-}
 func Test_Reload(t *testing.T) {
 	engine := NewFileSystem(http.Dir("./views"), ".amber")
 	engine.Reload(true) // Optional. Default: false
