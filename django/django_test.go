ca1b02c17098cab2f3d56bf7ea33c61e1d1e568d
SharkFourSix <18681730+SharkFourSix@users.noreply.github.com>
2022-07-16 01:01:18 +0200

    Add ability to forward base path to template engine to fix extended template resolution errors

--- django/django_test.go
+++ django/django_test.go
@@ -2,6 +2,7 @@ package django
 
 import (
 	"bytes"
+	"embed"
 	"io/ioutil"
 	"net/http"
 	"regexp"
@@ -102,6 +103,30 @@ func Test_FileSystem(t *testing.T) {
 		t.Fatalf("render: %v", err)
 	}
 	expect := `<!DOCTYPE html><html><head><title>Main</title></head><body><h2>Header</h2><h1>Hello, World!</h1><h2>Footer</h2></body></html>`
+	result := trim(buf.String())
+	if expect != result {
+		t.Fatalf("Expected:\n%s\nResult:\n%s\n", expect, result)
+	}
+}
+
+//go:embed views
+var viewsAsssets embed.FS
+
+func Test_PathForwardingFileSystem(t *testing.T) {
+	engine := NewPathForwardingFileSystem(http.FS(viewsAsssets), "/views", ".django")
+	engine.Debug(true)
+	if err := engine.Load(); err != nil {
+		t.Fatalf("load: %v\n", err)
+	}
+
+	var buf bytes.Buffer
+	err := engine.Render(&buf, "descendant", map[string]interface{}{
+		"greeting": "World",
+	})
+	if err != nil {
+		t.Fatalf("render: %v", err)
+	}
+	expect := `<h1>Hello World! from ancestor</h1>`
 	result := trim(buf.String())
 	if expect != result {
 		t.Fatalf("Expected:\n%s\nResult:\n%s\n", expect, result)
