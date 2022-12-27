32e9ac494297be543de54a0fa89e617a9403a6bc
adpande <adpande@gmail.com>
2022-12-27 16:31:01 +0530

    updated pongo2 from v4 to v6

--- django/django.go
+++ django/django.go
@@ -1,260 +1,260 @@
 package django
 
 import (
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-	"path/filepath"
-	"strings"
-	"sync"
-
-	"github.com/flosch/pongo2/v4"
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/template/utils"
+    "fmt"
+    "io"
+    "net/http"
+    "os"
+    "path/filepath"
+    "strings"
+    "sync"
+    
+    "github.com/flosch/pongo2/v6"
+    "github.com/gofiber/fiber/v2"
+    "github.com/gofiber/template/utils"
 )
 
 // Engine struct
 type Engine struct {
-	// delimiters
-	left  string
-	right string
-	// views folder
-	directory string
-	// http.FileSystem supports embedded files
-	fileSystem http.FileSystem
-	// views extension
-	extension string
-	// layout variable name that incapsulates the template
-	layout string
-	// determines if the engine parsed all templates
-	loaded bool
-	// reload on each render
-	reload bool
-	// debug prints the parsed templates
-	debug bool
-	// forward the base path to the template engine
-	forwardPath bool
-	// lock for funcmap and templates
-	mutex sync.RWMutex
-	// template funcmap
-	funcmap map[string]interface{}
-	// templates
-	Templates map[string]*pongo2.Template
+    // delimiters
+    left  string
+    right string
+    // views folder
+    directory string
+    // http.FileSystem supports embedded files
+    fileSystem http.FileSystem
+    // views extension
+    extension string
+    // layout variable name that incapsulates the template
+    layout string
+    // determines if the engine parsed all templates
+    loaded bool
+    // reload on each render
+    reload bool
+    // debug prints the parsed templates
+    debug bool
+    // forward the base path to the template engine
+    forwardPath bool
+    // lock for funcmap and templates
+    mutex sync.RWMutex
+    // template funcmap
+    funcmap map[string]interface{}
+    // templates
+    Templates map[string]*pongo2.Template
 }
 
 // New returns a Django render engine for Fiber
 func New(directory, extension string) *Engine {
-	engine := &Engine{
-		left:      "{{",
-		right:     "}}",
-		directory: directory,
-		extension: extension,
-		layout:    "embed",
-		funcmap:   make(map[string]interface{}),
-	}
-	return engine
+    engine := &Engine{
+        left:      "{{",
+        right:     "}}",
+        directory: directory,
+        extension: extension,
+        layout:    "embed",
+        funcmap:   make(map[string]interface{}),
+    }
+    return engine
 }
 
 func NewFileSystem(fs http.FileSystem, extension string) *Engine {
-	engine := &Engine{
-		left:       "{{",
-		right:      "}}",
-		directory:  "/",
-		fileSystem: fs,
-		extension:  extension,
-		layout:     "embed",
-		funcmap:    make(map[string]interface{}),
-	}
-	return engine
+    engine := &Engine{
+        left:       "{{",
+        right:      "}}",
+        directory:  "/",
+        fileSystem: fs,
+        extension:  extension,
+        layout:     "embed",
+        funcmap:    make(map[string]interface{}),
+    }
+    return engine
 }
 
 // NewPathForwardingFileSystem Passes "directory" to the template engine where alternative functions don't.
 // 							   This fixes errors during resolution of templates when "{% extends 'parent.html' %}" is used.
 func NewPathForwardingFileSystem(fs http.FileSystem, directory string, extension string) *Engine {
-	engine := NewFileSystem(fs, extension)
-	engine.forwardPath = true
-	engine.directory = directory
-	return engine
+    engine := NewFileSystem(fs, extension)
+    engine.forwardPath = true
+    engine.directory = directory
+    return engine
 }
 
 // Layout defines the variable name that will incapsulate the template
 func (e *Engine) Layout(key string) *Engine {
-	e.layout = key
-	return e
+    e.layout = key
+    return e
 }
 
 // Delims sets the action delimiters to the specified strings, to be used in
 // templates. An empty delimiter stands for the
 // corresponding default: {{ or }}.
 func (e *Engine) Delims(left, right string) *Engine {
-	fmt.Println("delims: this method is not supported for django")
-	return e
+    fmt.Println("delims: this method is not supported for django")
+    return e
 }
 
 // AddFunc adds the function to the template's function map.
 // It is legal to overwrite elements of the default actions
 func (e *Engine) AddFunc(name string, fn interface{}) *Engine {
-	e.mutex.Lock()
-	e.funcmap[name] = fn
-	e.mutex.Unlock()
-	return e
+    e.mutex.Lock()
+    e.funcmap[name] = fn
+    e.mutex.Unlock()
+    return e
 }
 
 // Reload if set to true the templates are reloading on each render,
 // use it when you're in development and you don't want to restart
 // the application when you edit a template file.
 func (e *Engine) Reload(enabled bool) *Engine {
-	e.reload = enabled
-	return e
+    e.reload = enabled
+    return e
 }
 
 // Debug will print the parsed templates when Load is triggered.
 func (e *Engine) Debug(enabled bool) *Engine {
-	e.debug = enabled
-	return e
+    e.debug = enabled
+    return e
 }
 
 // Parse is deprecated, please use Load() instead
 func (e *Engine) Parse() error {
-	fmt.Println("Parse() is deprecated, please use Load() instead.")
-	return e.Load()
+    fmt.Println("Parse() is deprecated, please use Load() instead.")
+    return e.Load()
 }
 
 // Load parses the templates to the engine.
 func (e *Engine) Load() error {
-	// race safe
-	e.mutex.Lock()
-	defer e.mutex.Unlock()
-
-	e.Templates = make(map[string]*pongo2.Template)
-
-	baseDir := e.directory
-
-	var pongoloader pongo2.TemplateLoader
-	if e.fileSystem != nil {
-		// ensures creation of httpFileSystemLoader only when filesystem is defined
-		if e.forwardPath {
-			pongoloader = pongo2.MustNewHttpFileSystemLoader(e.fileSystem, baseDir)
-		} else {
-			pongoloader = pongo2.MustNewHttpFileSystemLoader(e.fileSystem, "")
-		}
-	} else {
-		pongoloader = pongo2.MustNewLocalFileSystemLoader(baseDir)
-	}
-
-	// New pongo2 defaultset
-	pongoset := pongo2.NewSet("default", pongoloader)
-	// Set template settings
-	pongoset.Globals.Update(e.funcmap)
-	pongo2.SetAutoescape(false)
-
-	// Loop trough each directory and register template files
-	walkFn := func(path string, info os.FileInfo, err error) error {
-		// Return error if exist
-		if err != nil {
-			return err
-		}
-		// Skip file if it's a directory or has no file info
-		if info == nil || info.IsDir() {
-			return nil
-		}
-		// Skip file if it does not equal the given template extension
-		if len(e.extension) >= len(path) || path[len(path)-len(e.extension):] != e.extension {
-			return nil
-		}
-		// Get the relative file path
-		// ./views/html/index.tmpl -> index.tmpl
-		rel, err := filepath.Rel(e.directory, path)
-		if err != nil {
-			return err
-		}
-		// Reverse slashes '\' -> '/' and
-		// partials\footer.tmpl -> partials/footer.tmpl
-		name := filepath.ToSlash(rel)
-		// Remove ext from name 'index.tmpl' -> 'index'
-		name = strings.TrimSuffix(name, e.extension)
-		// name = strings.Replace(name, e.extension, "", -1)
-		// Read the file
-		// #gosec G304
-		buf, err := utils.ReadFile(path, e.fileSystem)
-		if err != nil {
-			return err
-		}
-		// Create new template associated with the current one
-		tmpl, err := pongoset.FromBytes(buf)
-		if err != nil {
-			return err
-		}
-		e.Templates[name] = tmpl
-		// Debugging
-		if e.debug {
-			fmt.Printf("views: parsed template: %s\n", name)
-		}
-		return err
-	}
-	// notify engine that we parsed all templates
-	e.loaded = true
-	if e.fileSystem != nil {
-		return utils.Walk(e.fileSystem, e.directory, walkFn)
-	}
-	return filepath.Walk(e.directory, walkFn)
+    // race safe
+    e.mutex.Lock()
+    defer e.mutex.Unlock()
+    
+    e.Templates = make(map[string]*pongo2.Template)
+    
+    baseDir := e.directory
+    
+    var pongoloader pongo2.TemplateLoader
+    if e.fileSystem != nil {
+        // ensures creation of httpFileSystemLoader only when filesystem is defined
+        if e.forwardPath {
+            pongoloader = pongo2.MustNewHttpFileSystemLoader(e.fileSystem, baseDir)
+        } else {
+            pongoloader = pongo2.MustNewHttpFileSystemLoader(e.fileSystem, "")
+        }
+    } else {
+        pongoloader = pongo2.MustNewLocalFileSystemLoader(baseDir)
+    }
+    
+    // New pongo2 defaultset
+    pongoset := pongo2.NewSet("default", pongoloader)
+    // Set template settings
+    pongoset.Globals.Update(e.funcmap)
+    pongo2.SetAutoescape(false)
+    
+    // Loop trough each directory and register template files
+    walkFn := func(path string, info os.FileInfo, err error) error {
+        // Return error if exist
+        if err != nil {
+            return err
+        }
+        // Skip file if it's a directory or has no file info
+        if info == nil || info.IsDir() {
+            return nil
+        }
+        // Skip file if it does not equal the given template extension
+        if len(e.extension) >= len(path) || path[len(path)-len(e.extension):] != e.extension {
+            return nil
+        }
+        // Get the relative file path
+        // ./views/html/index.tmpl -> index.tmpl
+        rel, err := filepath.Rel(e.directory, path)
+        if err != nil {
+            return err
+        }
+        // Reverse slashes '\' -> '/' and
+        // partials\footer.tmpl -> partials/footer.tmpl
+        name := filepath.ToSlash(rel)
+        // Remove ext from name 'index.tmpl' -> 'index'
+        name = strings.TrimSuffix(name, e.extension)
+        // name = strings.Replace(name, e.extension, "", -1)
+        // Read the file
+        // #gosec G304
+        buf, err := utils.ReadFile(path, e.fileSystem)
+        if err != nil {
+            return err
+        }
+        // Create new template associated with the current one
+        tmpl, err := pongoset.FromBytes(buf)
+        if err != nil {
+            return err
+        }
+        e.Templates[name] = tmpl
+        // Debugging
+        if e.debug {
+            fmt.Printf("views: parsed template: %s\n", name)
+        }
+        return err
+    }
+    // notify engine that we parsed all templates
+    e.loaded = true
+    if e.fileSystem != nil {
+        return utils.Walk(e.fileSystem, e.directory, walkFn)
+    }
+    return filepath.Walk(e.directory, walkFn)
 }
 
 func getPongoBinding(binding interface{}) pongo2.Context {
-	if binding == nil {
-		return nil
-	}
-	if binds, ok := binding.(pongo2.Context); ok {
-		return binds
-	}
-	if binds, ok := binding.(map[string]interface{}); ok {
-		return binds
-	}
-	if binds, ok := binding.(fiber.Map); ok {
-		bind := make(pongo2.Context)
-		for key, value := range binds {
-			bind[key] = value
-		}
-		return bind
-	}
-
-	return nil
+    if binding == nil {
+        return nil
+    }
+    if binds, ok := binding.(pongo2.Context); ok {
+        return binds
+    }
+    if binds, ok := binding.(map[string]interface{}); ok {
+        return binds
+    }
+    if binds, ok := binding.(fiber.Map); ok {
+        bind := make(pongo2.Context)
+        for key, value := range binds {
+            bind[key] = value
+        }
+        return bind
+    }
+    
+    return nil
 }
 
 // Render will render the template by name
 func (e *Engine) Render(out io.Writer, template string, binding interface{}, layout ...string) error {
-	if !e.loaded || e.reload {
-		if e.reload {
-			e.loaded = false
-		}
-		if err := e.Load(); err != nil {
-			return err
-		}
-	}
-	tmpl, ok := e.Templates[template]
-	if !ok {
-		return fmt.Errorf("template %s does not exist", template)
-	}
-
-	bind := getPongoBinding(binding)
-	parsed, err := tmpl.Execute(bind)
-	if err != nil {
-		return err
-	}
-	if len(layout) > 0 && layout[0] != "" {
-		if bind == nil {
-			bind = make(map[string]interface{}, 1)
-		}
-		bind[e.layout] = parsed
-		lay := e.Templates[layout[0]]
-		if lay == nil {
-			return fmt.Errorf("layout %s does not exist", layout[0])
-		}
-		return lay.ExecuteWriter(bind, out)
-	}
-	if _, err = out.Write([]byte(parsed)); err != nil {
-		return err
-	}
-	return nil
+    if !e.loaded || e.reload {
+        if e.reload {
+            e.loaded = false
+        }
+        if err := e.Load(); err != nil {
+            return err
+        }
+    }
+    tmpl, ok := e.Templates[template]
+    if !ok {
+        return fmt.Errorf("template %s does not exist", template)
+    }
+    
+    bind := getPongoBinding(binding)
+    parsed, err := tmpl.Execute(bind)
+    if err != nil {
+        return err
+    }
+    if len(layout) > 0 && layout[0] != "" {
+        if bind == nil {
+            bind = make(map[string]interface{}, 1)
+        }
+        bind[e.layout] = parsed
+        lay := e.Templates[layout[0]]
+        if lay == nil {
+            return fmt.Errorf("layout %s does not exist", layout[0])
+        }
+        return lay.ExecuteWriter(bind, out)
+    }
+    if _, err = out.Write([]byte(parsed)); err != nil {
+        return err
+    }
+    return nil
 }
